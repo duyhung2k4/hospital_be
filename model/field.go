295aad4cc61ec782b742922fccfee81d8e85f6ca
ndhung <nguyenduyhung04092004@gmail.com>
2024-09-26 15:20:22 +0700

    fix 1 chut

--- model/field.go
+++ model/field.go
@@ -6,8 +6,8 @@ type Field struct {
 	gorm.Model
 	Lable         string `json:"lable"`
 	Name          string `json:"name" gorm:"uniqueIndex:name_department_idx"`
-	Type          string `json:"type"` // int - float - text - select
-	DefaultValues string `json:"defaultValues"`
+	Type          string `json:"type"`          // int - float - text - select
+	DefaultValues string `json:"defaultValues"` // json string []{ key: string, value: string }
 
 	DepartmentId uint        `json:"departmentId" gorm:"uniqueIndex:name_department_idx"`
 	Department   *Department `json:"department" gorm:"foreignKey:DepartmentId; constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
