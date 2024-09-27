ba3589d7dabddd23ba64f511928cc866ddb28d80
ndhung <nguyenduyhung04092004@gmail.com>
2024-09-28 02:10:04 +0700

    field

--- model/field.go
+++ model/field.go
@@ -7,12 +7,12 @@ import (
 
 type Field struct {
 	gorm.Model
-	Lable         string         `json:"lable"`
+	Label         string         `json:"label"`
 	Placeholder   string         `json:"placeholder"`
 	Name          string         `json:"name" gorm:"uniqueIndex:name_department_idx"`
 	Size          int            `json:"size"`                             // width: 1 - 12
 	Type          string         `json:"type"`                             // int - float - text - area - select
-	DefaultValues pq.StringArray `json:"defaultValues" gorm:"type:text[]"` // string []{ lable: string, key: string }
+	DefaultValues pq.StringArray `json:"defaultValues" gorm:"type:text[]"` // string []{ label: string, value: string }
 
 	DepartmentId uint        `json:"departmentId" gorm:"uniqueIndex:name_department_idx"`
 	Department   *Department `json:"department" gorm:"foreignKey:DepartmentId; constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
